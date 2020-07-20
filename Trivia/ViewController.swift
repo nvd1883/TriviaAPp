8a755c0936428daf5b346f3b4e2f04e2bef84de7
Nived <nived_pradeep@yahoo.in>
2020-07-20 18:39:27 +0530

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Aspire on 20/07/20.
+//  Copyright © 2020 Aspire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
